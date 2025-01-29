c4785bd2a9fb03a6288378c0b5042a3b2d6e5ca5
HisaCat <ahisacat@gmail.com>
2025-01-29 22:57:21 +0900

    Supports disable domain reloading

--- Assets/Dissonance/Integrations/FishNet/DissonanceFishNetComms.cs
+++ Assets/Dissonance/Integrations/FishNet/DissonanceFishNetComms.cs
@@ -12,7 +12,19 @@ namespace Dissonance.Integrations.FishNet
 	// Comms implementation for Dissonance Voice. Used Unit, due to FishNet is connected while using Dissonance Voice
 	public sealed class DissonanceFishNetComms : BaseCommsNetwork<DissonanceFishNetServer, DissonanceFishNetClient, DissonanceFishNetConnection, Unit, Unit>
 	{
-		public static DissonanceFishNetComms Instance { get; private set; }
+        public static DissonanceFishNetComms Instance { get; private set; }
+#if UNITY_EDITOR
+#pragma warning disable IDE0051
+        [UnityEditor.InitializeOnEnterPlayMode]
+        private static void OnEnterPlaymodeInEditor(UnityEditor.EnterPlayModeOptions options)
+        {
+            if (options.HasFlag(UnityEditor.EnterPlayModeOptions.DisableDomainReload))
+            {
+                Instance = null;
+            }
+        }
+#pragma warning restore IDE0051
+#endif
 
 		public DissonanceComms Comms { get; private set; }
         internal NetworkManager NetworkManager { get; private set; }
--- Assets/Dissonance/Integrations/FishNet/Utils/LoggingHelper.cs
+++ Assets/Dissonance/Integrations/FishNet/Utils/LoggingHelper.cs
@@ -5,7 +5,20 @@ namespace Dissonance.Integrations.FishNet.Utils
     // Helper class for Dissonance events logging
     internal static class LoggingHelper
     {
-        public static readonly Log Logger = Logs.Create(LogCategory.Network, "FishNet");
+        public static Log _logger = Logs.Create(LogCategory.Network, "FishNet");
+        public static Log Logger => _logger;
+#if UNITY_EDITOR
+#pragma warning disable IDE0051
+        [UnityEditor.InitializeOnEnterPlayMode]
+        private static void OnEnterPlaymodeInEditor(UnityEditor.EnterPlayModeOptions options)
+        {
+            if (options.HasFlag(UnityEditor.EnterPlayModeOptions.DisableDomainReload))
+            {
+                _logger = Logs.Create(LogCategory.Network, "FishNet");
+            }
+        }
+#pragma warning restore IDE0051
+#endif
 
         private const string RunningAsTemplate = "Running as: {0}!";
         private const string StoppingAsTemplate = "Stopping as: {0}!";
